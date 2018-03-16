4c7bdf82cc57547876404f9ab00a7c9decb6a4ba
trial-user <toptrop18@gmail.com>
2018-03-15 20:05:27 -0400

    Adds Doc Strings

--- caduceus/sorter.py
+++ caduceus/sorter.py
@@ -1,10 +1,14 @@
-class SnakeSorter:
-    def __init__(self, snakes):
+class SnakeSorter:   
+ def __init__(self, snakes):
+"""initializes snake sorter"""
         self.snakes = snakes
         self.sorted_snakes = []
 
     def sort_by_weight(self):
+"""sorts snakes by weight"""
         self.sorted_snakes = sorted(self.snakes, key=lambda snek: snek.weight)
 
     def sort_by_length(self):
+"""sorts snakes by length"""
         self.sorted_snakes = sorted(self.snakes, key=lambda snek: snek.length)
+
